4d4f8c3e6dadf26f527ca485390306def2d264dc
DevVivekk <vivekbhardwajvk2202@gmail.com>
2022-12-26 23:26:43 +0530

    removed 4000

--- server/client/src/subelem/photoelem.js
+++ server/client/src/subelem/photoelem.js
@@ -1,4 +1,4 @@
-import React, {  useEffect, useState } from 'react'
+import React, {   useState } from 'react'
 import Photos from '../updates/photos'
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
